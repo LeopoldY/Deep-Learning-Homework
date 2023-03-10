6bdcc5d161d24a3f361327a7da0f2ee8fd5db606
Leopold Yang <1480335580@qq.com>
2023-03-10 08:51:17 +0800

    main

--- homework1.py
+++ homework1.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 def f(x):
     return np.sin(x)
 
+# 数值微分法
 def numerical_diff(x):
     h = 0.0001
     return (f(x+h) - f(x-h)) / (2*h)
@@ -15,14 +16,22 @@ x = np.arange(-5.0, 5.0, 0.1)
 y = f(x)
 dy_num = numerical_diff(x)
 
+# 自动求导
 x_torch = torch.tensor(x, requires_grad=True)
 y_torch = torch.sin(x_torch)
 y_torch.sum().backward()
 dy_auto = x_torch.grad
 
-plt.plot(x, y, label="f(x)")
-plt.plot(x, dy_num, label="f'(x) (numerical)")
-plt.plot(x, dy_auto, label="f'(x) (automatic)")
-plt.legend()
-plt.show()
+fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, figsize=(8, 8))
+
+ax1.plot(x, y, label="f(x)")
+ax1.legend()
+
+ax2.plot(x, dy_num, label="f'(x) (numerical)")
+ax2.legend()
+
+ax3.plot(x, dy_auto, label="f'(x) (automatic)")
+ax3.legend()
+
+plt.show() 
 
